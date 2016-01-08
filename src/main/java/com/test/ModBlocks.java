abb693e350aede87c51058c1c4cbf8a60a97241d
CircuitLord <circuitlordmc@gmail.com>
2016-01-08 12:54:58 -0600

    Simple changes.

--- src/main/java/com/test/ModBlocks.java
+++ src/main/java/com/test/ModBlocks.java
@@ -17,8 +17,10 @@ public class ModBlocks {
 
     	//block(name) = (Block(name)) new Block(name)().setUnlocalizedName("block(name)");   	
     	//GameRegistry.registerBlock(block(name), "block(name)");
+    	
     	blockChoclate = (BlockChoclate) new BlockChoclate().setUnlocalizedName("blockChoclate");   	
     	GameRegistry.registerBlock(blockChoclate, "blockChoclate");
+    	
     	blockX = (BlockX) new BlockX().setUnlocalizedName("blockX");   	
     	GameRegistry.registerBlock(blockX, "blockX");
     	
