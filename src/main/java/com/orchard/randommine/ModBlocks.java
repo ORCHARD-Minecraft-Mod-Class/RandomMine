400810d091cf5380311f7c6480b1a61ba86a350f
ParkerMc01 <nick@reisingerfamily.net>
2016-01-21 17:02:24 -0600

    changed the texture a little to look more real good job @LOTRfan02

--- src/main/java/com/orchard/randommine/ModBlocks.java
+++ src/main/java/com/orchard/randommine/ModBlocks.java
@@ -30,7 +30,7 @@ public class ModBlocks {
     	BlockFaceSilly = (BlockFaceSilly) new BlockFaceSilly().setUnlocalizedName("BlockFaceSilly").setCreativeTab(RandomMine.tab);   	
     	GameRegistry.registerBlock(BlockFaceSilly, "BlockFaceSilly");
     	
-    	blockSwordintheStone = (BlockSwordintheStone) new BlockSwordintheStone().setUnlocalizedName("blockSwordintheStone");   	
+    	blockSwordintheStone = (BlockSwordintheStone) new BlockSwordintheStone().setUnlocalizedName("blockSwordintheStone").setCreativeTab(RandomMine.tab);   	
     	GameRegistry.registerBlock(blockSwordintheStone, "blockSwordintheStone");
     	
     	
