a1130622d966d35b05f4c73805ccadd8a468c3af
PieCrafter314 <zfam1.pjz@gmail.com>
2016-03-04 13:14:41 -0600

    Added Stuff

--- src/main/java/com/orchard/randommine/ItemRendering.java
+++ src/main/java/com/orchard/randommine/ItemRendering.java
@@ -31,6 +31,14 @@ public class ItemRendering {
 	registerItemRendering("PurpleArmor_boots");
 	registerItemRendering("PurpleArmor_body");
 	registerItemRendering(":PurpleArmor_head");
+	registerItemRendering("Minion_Armor_leggins");
+	registerItemRendering("Minion_Armor_boots");
+	registerItemRendering("Minion_Armor_body");
+	registerItemRendering(":Minion_Armor_head");
+	registerItemRendering("Techno_Armor_leggins");
+	registerItemRendering("Techno_Armor_boots");
+	registerItemRendering("Techno_Armor_body");
+	registerItemRendering(":Techno_Armor_head");
 //github.com/ORCHARD-Minecraft-Mod-Class/mod-repo-test.git
 	}
 }
---
+++ src/main/java/com/orchard/randommine/armor/Minion_Armor.java
@@ -0,0 +1,51 @@
+package com.orchard.randommine.armor;
+import net.minecraftforge.fml.common.event.*;
+import net.minecraft.creativetab.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.item.*;
+import net.minecraft.world.*;
+import java.util.Random;
+
+import net.minecraftforge.common.util.*;
+public class Minion_Armor{
+
+public Minion_Armor(){}
+
+public static Item helmet;
+public static Item body;
+public static Item legs;
+public static Item boots;
+public Object instance;
+public void generateNether(World world, Random random, int chunkX, int chunkZ){}
+public void generateSurface(World world, Random random, int chunkX, int chunkZ){}
+public int addFuel(ItemStack fuel){return 0;}
+public void serverLoad(FMLServerStartingEvent event){}
+public void preInit(FMLPreInitializationEvent event){}
+public void registerRenderers(){}
+
+
+static{
+ItemArmor.ArmorMaterial enuma = EnumHelper.addArmorMaterial("MINIONARMOR", "Minion_Armor", 25, new int[] {2, 7, 5, 3}, 42);
+
+int armorPreffix = 0;
+helmet = (new ItemArmor(enuma, armorPreffix, 0){public void onArmorTick(World world, EntityPlayer entity, ItemStack itemStack){
+}
+}).setUnlocalizedName("Minion_Armor_head");helmet.setMaxStackSize(1);
+body = (new ItemArmor(enuma, armorPreffix, 1){public void onArmorTick(World world, EntityPlayer entity, ItemStack itemStack){
+}
+}).setUnlocalizedName("Minion_Armor_body");body.setMaxStackSize(1);
+legs = (new ItemArmor(enuma, armorPreffix, 2){public void onArmorTick(World world, EntityPlayer entity, ItemStack itemStack){
+}
+}).setUnlocalizedName("Minion_Armor_leggins");legs.setMaxStackSize(1);
+boots = (new ItemArmor(enuma, armorPreffix, 3){public void onArmorTick(World world, EntityPlayer entity, ItemStack itemStack){
+}
+}).setUnlocalizedName("Minion_Armor_boots");boots.setMaxStackSize(1);
+
+helmet.setCreativeTab(CreativeTabs.tabCombat);
+body.setCreativeTab(CreativeTabs.tabCombat);
+legs.setCreativeTab(CreativeTabs.tabCombat);
+boots.setCreativeTab(CreativeTabs.tabCombat);
+
+}
+
+}
